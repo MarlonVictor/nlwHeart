69211db10dc6afed118b1d07d31b1314e0fed8f7
MarlonVictor <cmarlonvictor11@gmail.com>
2021-10-20 12:21:30 -0300

    Using vite to start frontend with initial settings

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main className="bg-gray-900 h-screen">
+            <h1 className="text-2xl font-bold text-gray-400">DoWhile</h1>
+            <p className="text-red-600">Inicio: 02/12/2021</p>
+        </main>
+    )
+}
+
+export default App
---
+++ web/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    purge: [],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    450: '#1B873F'
+                },
+                pink: {
+                    450: '#FF008E'
+                },
+                yellow: {
+                    450: '#FFCD1E',
+                    550: '#FF7A29'
+                }
+            }
+        }
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
