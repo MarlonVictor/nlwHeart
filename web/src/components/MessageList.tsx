a8e45e0db9d77d1e96bf60b0e5e0fc7c037272a0
MarlonVictor <cmarlonvictor11@gmail.com>
2021-10-27 21:25:42 -0300

    Updating message list and adding message to db

--- web/src/components/MessageList.tsx
+++ web/src/components/MessageList.tsx
@@ -45,16 +45,16 @@ export function MessageList() {
     }, [])
 
     return (
-        <section id="Opinions" className="h-screen max-w-6xl mx-auto pt-16">
-            <ul className="mt-7" style={{ columns: '3', columnGap: '10px', height: '90%' }}>
+        <section id="Opinions" className="h-auto max-w-6xl mx-auto pt-16 pb-24 bg-home-background">
+            <ul className="mt-6" style={{ columns: '3', columnGap: '16px', height: '90%' }}>
                 {messages.length > 0 && messages.map((message, key) => {
                     return (
                     <li 
                             key={key} 
-                            className="bg-black-750 w-full mb-2 p-3 overflow-hidden rounded-md"
+                            className="bg-black-750 w-full mb-4 p-5 overflow-hidden rounded-sm"
                             style={{ breakInside: 'avoid' }}
                         >
-                            <header className="flex gap-3 items-center mb-4">
+                            <header className="flex gap-3 items-center pb-5 mb-5 border-b-2 border-opacity-10">
                                 <div className="p-px bg-gradient-to-r from-pink-450 to-yellow-450 rounded-full leading-none">
                                     <img 
                                         src={message.user.avatar_url} 
@@ -62,7 +62,7 @@ export function MessageList() {
                                         className="w-16 h-16 rounded-full border-4 border-black-750"
                                     />
                                 </div>
-                                <h3 className="font-bold">{message.user.name}</h3>
+                                <h3 className="font-bold text-xl">{message.user.name}</h3>
                             </header>
                             <p>"{message.text}"</p>
                     </li>
